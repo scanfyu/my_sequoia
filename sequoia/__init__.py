df9f1524e6758f0baa4341a58a72ffbe4e93b446
Yu <songyupm@foxmail.com>
2022-09-23 08:27:58 +0800

    优化路径

---
+++ sequoia/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''=================================================
+@Project :my_sequoia
+@FileName:__init__.py 
+@Python  :Python 3.9+
+@Time    :2022/9/22 23:16:18
+@Desc    :
+=================================================='''
+
+if __name__ == '__main__':
+    pass
