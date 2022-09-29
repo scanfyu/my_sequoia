db26312545dfd129bbf0ffb0dafacea6671a6ee2
Yu <songyupm@foxmail.com>
2022-09-29 18:20:09 +0800

    高频行情分析

--- sequoia/multi_periods_trade/bar_1m_base.py
+++ sequoia/multi_periods_trade/bar_1m_base.py
@@ -14,6 +14,7 @@ from czsc.gms.gm_base import format_kline
 from czsc.enum import Freq
 import pandas as pd
 import numpy as np
+import time
 
 
 def kline_raw_bar(df_slice):
@@ -26,28 +27,59 @@ def loop_bar():
     播放历史bar数据
     :return:
     """
-    # 全局笔数据
-    g_b_number = 0
-    # 使用一个列表存储全部的笔
-    g_b_content = []
-    # 笔的方向
-    bis_dir = []
+    t1 = time.time()
+
+    df_status = pd.DataFrame()
+    n_close = 0
 
     # 读取本地的数据
     rb_bar = read_local()
     for i in range(len(rb_bar)):
         # 轮询播放bar数据
-        raw_bars = kline_raw_bar(rb_bar.iloc[i:i+1])
+        raw_bars = kline_raw_bar(rb_bar.iloc[i:i + 1])
+        raw_bar = raw_bars[-1]
 
-        c = CZSC(raw_bars)
-        if len(c.bi_list) > g_b_number:
-            # 将笔数据缓存
-            g_b_content.append(c.bi_list[-1])
-            bis_dir.append(c.bi_list[-1].direction)
+        if i == 0:
+            c = CZSC(raw_bars)
+            n_return = 0
+        else:
+            c.update(raw_bar)
+            n_return = np.log(n_close/raw_bar.close)
 
-        # 1.衍生多周期数据；2.笔的划分
-        # 实时的数据传进来，丢到数据流里建模
+        # -- 特征值
+        dt_ = raw_bar.dt
+        # 最新价
+        last_p = raw_bar.close
+        # 单一bar涨跌幅
+        r_d = (raw_bar.close / raw_bar.open - 1) * 100
+        n_close = raw_bar.close
+        # czsc对象
+        # 未成笔的K线数量
+        b_ubi = len(c.bars_ubi)
+        # 成笔的数量
+        b_lgh = len(c.bi_list)
+        # 最新一笔的方向
+        if b_lgh > 0:
+            l_bi_dir = c.bi_list[-1].direction
+            last_bi = c.bi_list[-1]
+        else:
+            l_bi_dir = "无"
+            last_bi = None
+
+        var_c = [last_p, dt_, r_d, n_return, b_ubi, b_lgh, l_bi_dir, last_bi]
+        slice_t = pd.DataFrame(var_c).T
+
+        df_status = pd.concat([df_status, slice_t])
+
+    df_status.rename({"0": "price", "1": "time", "2": "b_pchg", "3": "pchg", "4": "ubi_n", "5": "bi_lg",
+                      "6": "bi_dir", "7": "last_bi"})
+    df_status.to_csv("D:/data/czsc_ana/rb_1m.csv")
+
+    t_spend = time.time() - t1
+    print(f"spend all {round(t_spend, 2)}, {round(t_spend/len(rb_bar), 5)} per once.")
 
 
 if __name__ == '__main__':
+
     loop_bar()
+
